e0a66495ddfdc79fd14ba1c39367dd9d7b213e87
Master <enseladajoaq@gmail.com>
2026-04-02 19:50:00 +0800

    Seo new features

--- swish-ag/astro.config.mjs
+++ swish-ag/astro.config.mjs
@@ -1,12 +1,12 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  // output: 'server' -- remove this line
+  site: 'https://swish-tawny.vercel.app',
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
